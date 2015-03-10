2e97891a31b507b16a028116b5bc41204b52889b
John Lees <lees.john6@gmail.com>
2015-03-10 12:26:15 +0000

    Nice version of command line options

--- src/pangwas.h
+++ src/pangwas.h
@@ -12,6 +12,8 @@
 #include <string>
 #include <list>
 #include <vector>
+#include <thread>
+#include <sys/stat.h>
 
 // Boost headers
 #include <boost/program_options.hpp>
@@ -20,10 +22,12 @@
 //    Default options
 const long int max_length_default = 100;
 const double maf_default = 0.01;
-const std::string chi2_default = "1e-5";
-const std::string pval_default = "1e-8";
+const std::string chi2_default = "10e-5";
+const std::string pval_default = "10e-8";
 
 // pangwasMain headers
-int parseCommandLine (int argc, char *argv[], boost::program_options::variables_map *vm);
-void printHelp(boost::program_options::options_description *help);
+int parseCommandLine (int argc, char *argv[], boost::program_options::variables_map& vm);
+
+void printHelp(boost::program_options::options_description& help);
+int fileStat(const std::string& filename);
 
--- src/pangwasMain.cpp
+++ src/pangwasMain.cpp
@@ -14,44 +14,59 @@ int main (int argc, char *argv[])
    // Program description
    std::cerr << "pangwas: pan-genome wide association study using kmers\n";
 
-   po::variables_map vm;
-
    // Do parsing and checking of command line params
    // If no input options, give quick usage rather than full help
+   po::variables_map vm;
    if (argc == 1)
    {
       std::cerr << "Usage: pangwas -k dsm.txt -p data.pheno -o output\n\n"
          << "For full option details run pangwas -h\n";
       return 0;
    }
-   else if (parseCommandLine(argc, argv, &vm))
+   else if (parseCommandLine(argc, argv, vm))
    {
       return 1;
    }
 
-   if (vm.count("compression"))
+   if (vm.count("kmers"))
    {
-      std::cout << "Compression level was set to " << vm["compression"].as<int>() << ".\n";
+      std::cout << "kmers was set to " << vm["kmers"].as<std::string>() << ".\n";
    }
    else
    {
       std::cout << "Compression level was not set.\n";
    }
-}
+
+   // Open .pheno file, parse into sensible object (vector of samples?)
+
+   // Thread from here...
+   // Parse a dsm line
+
+   // Filter
+
+   // Test
+
+   // Print
+   // ...to here
+
+   }
 
 // Use boost::program_options to parse command line input
-int parseCommandLine (int argc, char *argv[], po::variables_map *vm)
+// This does pretty much all the parameter checking needed
+int parseCommandLine (int argc, char *argv[], po::variables_map& vm)
 {
    int failed = 0;
 
-   //TODO look at vector string vs. string
    //Required options
    po::options_description required("Required options");
    required.add_options()
-    ("help,h", "full help message")
     ("kmers,k", po::value<std::string>()->required(), "dsm kmer output file")
     ("pheno,p", po::value<std::string>()->required(), ".pheno metadata")
     ("output,o", po::value<std::string>()->required(), "output prefix");
+
+   po::options_description other("Other options");
+   other.add_options()
+    ("help,h", "full help message");
 
    //Optional filtering parameters
    //NB pval cutoffs are strings for display, and are converted to floats later
@@ -64,29 +79,36 @@ int parseCommandLine (int argc, char *argv[], po::variables_map *vm)
     ("pval", po::value<std::string>()->default_value(pval_default), "p-value threshold for final logistic test");
 
    po::options_description all;
-   all.add(required).add(filtering);
+   all.add(required).add(filtering).add(other);
 
    try
    {
-      po::store(po::command_line_parser(argc, argv).options(all).run(), *vm);
+      po::store(po::command_line_parser(argc, argv).options(all).run(), vm);
 
-      if (vm->count("help"))
+      if (vm.count("help"))
       {
-         printHelp(&all);
+         printHelp(all);
          failed = 1;
       }
       else
       {
-         po::notify(*vm);
+         po::notify(vm);
          failed = 0;
+
+         // Check input files exist, and can stat
+         if (!fileStat(vm["kmers"].as<std::string>()) || !fileStat(vm["pheno"].as<std::string>()))
+         {
+            failed = 1;
+         }
       }
 
    }
    catch (po::error& e)
    {
+      // Report errors from boost library
       std::cerr << "Error in command line input: " << e.what() << "\n";
       std::cerr << "Run 'pangwas --help' for full option listing\n\n";
-      std::cerr << required << "\n";
+      std::cerr << required << "\n" << other << "\n";
 
       failed = 1;
    }
@@ -95,7 +117,23 @@ int parseCommandLine (int argc, char *argv[], po::variables_map *vm)
 }
 
 // Print long help message
-void printHelp(po::options_description *help)
+void printHelp(po::options_description& help)
 {
-   std::cerr << *help << "\n";
+   std::cerr << help << "\n";
+}
+
+// Check for file existence
+int fileStat(const std::string& filename)
+{
+   struct stat buffer;
+   int success = 1;
+
+   if (stat (filename.c_str(), &buffer) != 0)
+   {
+      std::cerr << "Can't stat input file: " << filename << "\n";
+
+      success = 0;
+   }
+
+   return success;
 }
