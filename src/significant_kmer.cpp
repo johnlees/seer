johnlees/seer

commit 968ff0347a1bef5399dfbff3437204d278cccbef

map_back to be compatible with f50de15
Need to read seer output differently, as now all on one line

src/significant_kmer.cpp
@@ -24,17 +24,21 @@ std::istream& operator>>(std::istream &is, Significant_kmer& sk)
    std::string sequence, sample, line_in = "";
    std::vector<std::string> sample_list;
 
-   // Read the line with sequence and p-value, extract only sequence
+   // Read the line convert to stringstream, extract only sequence
    std::getline(is, line_in);
    std::stringstream line_stream(line_in);
 
    line_stream >> sequence;
 
-   // Read the line with samples, extract using the same format as they were
-   // written with
-   std::getline(is, line_in);
-   line_stream.str(line_in);
+   // Ignore fields with p-values, betas etc. Number to ignore set in header
+   // file
+   for (unsigned int i = 0; i < ignored_fields; ++i)
+   {
+      line_stream.ignore(std::numeric_limits<std::streamsize>::max(), ' ');
+   }
 
+   // Read remainder of line, which is sample names, in the same way they were
+   // written
    std::copy(std::istream_iterator<std::string>(line_stream), std::istream_iterator<std::string>(), std::back_inserter(sample_list));
 
    // Ensure vector remains sorted on sample name

src/significant_kmer.hpp
@@ -11,6 +11,8 @@
 #include <iterator>
 #include <algorithm>
 
+const unsigned int ignored_fields = 3;
+
 // kmers have a sequence, and a list of samples they appear in
 class Significant_kmer
 {

commit 4f406ce6ab626ea00a1c6a9e31e1758278772d4c

Fix bug introduced in 968ff03
Correct field separator
Ignore correct number of fields

src/significant_kmer.cpp
@@ -31,10 +31,10 @@ std::istream& operator>>(std::istream &is, Significant_kmer& sk)
    line_stream >> sequence;
 
    // Ignore fields with p-values, betas etc. Number to ignore set in header
-   // file
-   for (unsigned int i = 0; i < ignored_fields; ++i)
+   // file. Add one to this as first tab will be matched by ignore
+   for (unsigned int i = 0; i < ignored_fields + 1; ++i)
    {
-      line_stream.ignore(std::numeric_limits<std::streamsize>::max(), ' ');
+      line_stream.ignore(std::numeric_limits<std::streamsize>::max(), '\t');
    }
 
    // Read remainder of line, which is sample names, in the same way they were