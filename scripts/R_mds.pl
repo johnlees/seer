johnlees/seer

commit 9882e0b4ea93d7756249456cd3ba4a819a923eba

Add scree plot into R_mds.pl
Will output to scree_plot.pdf

Closes #18

scripts/R_mds.pl
@@ -111,7 +111,7 @@ else
    my $Rscript = <<RSCRIPT;
 library(rhdf5)
 distances <- read.csv("$distance_file", header=FALSE, stringsAsFactors=FALSE)
-projection <- cmdscale(distances, k = $dimensions)
+projection <- cmdscale(distances, k = $dimensions, eig = T)
 
 for (i in 1:$dimensions)
 {
@@ -121,6 +121,10 @@ for (i in 1:$dimensions)
 h5createFile("$output_prefix")
 h5write(projection,"$output_prefix","dataset")
 H5close()
+
+pdf("scree_plot.pdf")
+plot(projection[[2]][1:30], type='l', xlab="Dimensions", ylab="Eigenvalue", main="Scree plot")
+dev.off()
 RSCRIPT
 
    open(RSCRIPT, ">$tmp_file_name") || die("Could not open $tmp_file_name: $!\n");

commit acc4bc17f9da0559ec552cca111f314bb7ee91ec

Fix R_mds.pl script
Error introduced in 9882e0b4ea93d7756249456cd3ba4a819a923eba when plot
was added

scripts/R_mds.pl
@@ -112,14 +112,15 @@ else
 library(rhdf5)
 distances <- read.csv("$distance_file", header=FALSE, stringsAsFactors=FALSE)
 projection <- cmdscale(distances, k = $dimensions, eig = T)
+covariates <- projection[["points"]]
 
 for (i in 1:$dimensions)
 {
-   projection[,i] <- projection[,i]/max(abs(projection[,i]))
+   covariates[,i] <- covariates[,i]/max(abs(covariates[,i]))
 }
 
 h5createFile("$output_prefix")
-h5write(projection,"$output_prefix","dataset")
+h5write(covariates,"$output_prefix","dataset")
 H5close()
 
 pdf("scree_plot.pdf")